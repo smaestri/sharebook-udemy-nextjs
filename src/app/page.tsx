a1374e1029c02fdcf2fe9bfb757f009c8530892a
Sylvain Maestri <sylvainmaestri@gmail.com>
2024-01-23 21:49:51 +0100

    add link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import Link from "next/link";
 
 export default async function Home() {
-  return <div>Accueil</div>
+  return <div>
+    <Link href="/">Home</Link>
+    <Link href="my-books">Mes livres</Link>
+    <Link href="list-books">livres</Link>*
+    <Link href="borrows">Mes emprunts</Link>
+    <Link href="search">Recherche</Link>
+
+    </div>
 }
  
