1aa30a82b3049cd8daeb3c265aea5148d6837dc7
Sylvain Maestri <sylvainmaestri@gmail.com>
2024-01-29 21:09:05 +0100

    afficher mes livres

--- src/app/my-books/page.tsx
+++ src/app/my-books/page.tsx
@@ -1,9 +1,25 @@
+import { db } from "@/lib/db";
+import { Book } from "@prisma/client";
 import Link from "next/link";
+import Image from "next/image"
+
 
 export default async function MyBooksPage() {
+  const books = await db.book.findMany();
+  console.log('books', books)
+  const renderBooks = () => (
+    books.map((book: Book) => (<div>
+      <Image src="" alt="book" />
+      <div>{book.title}</div>
+      <div>{book.author}</div>
+    </div>))
+  )
+
   return <>
-    <h1  className="text-2xl">Mes Livres</h1>
+    <h1 className="text-2xl">Mes Livres</h1>
+    {renderBooks()}
     <Link href="/my-books/new">
+
       <button>Créer un livre</button>
     </Link>
   </>
