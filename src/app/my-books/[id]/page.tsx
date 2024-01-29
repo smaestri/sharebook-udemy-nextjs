35efa11ce00cb7037118770ee39af167dc2b57aa
Sylvain Maestri <sylvainmaestri@gmail.com>
2024-01-29 22:11:47 +0100

    notfound

--- src/app/my-books/[id]/page.tsx
+++ src/app/my-books/[id]/page.tsx
@@ -1,6 +1,7 @@
 import BookForm from "@/components/book-form";
 import { db } from "@/lib/db";
 import { Book } from "@prisma/client";
+import { notFound } from "next/navigation";
 
 interface EditBookProps {
     params: {
@@ -14,6 +15,10 @@ export default async function EditBookPage(props: EditBookProps) {
     const book: Book = await db.book.findFirst({
         where: { id }
     })
+
+    if(!book) {
+        return notFound();
+    }
 
     return (
         <>
