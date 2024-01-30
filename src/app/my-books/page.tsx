b97eaf1295a28480562700c7e05a374e58bda5fe
Sylvain Maestri <sylvainmaestri@gmail.com>
2024-01-30 22:36:51 +0100

    style - mes livres

--- src/app/my-books/page.tsx
+++ src/app/my-books/page.tsx
@@ -2,27 +2,41 @@ import { db } from "@/lib/db";
 import { Book } from "@prisma/client";
 import Link from "next/link";
 import Image from "next/image"
+import bookImg from '/public/book.png'
+
 
 export default async function MyBooksPage() {
   const books = await db.book.findMany();
   console.log('books', books)
   const renderBooks = () => (
-    books.map((book: Book) => (<div>
-      <Image src="" alt="book" />
-      <div>{book.id}</div>
-      <div>{book.title}</div>
-      <div>{book.author}</div>
-      <div> <Link href={`/my-books/${book.id}`}>
-        <button>Modifier</button>
-      </Link></div>
-    </div>))
+    books.map((book: Book) => (
+      <div
+        key={book.id}
+        className="flex flex-col m-5"
+      >
+        <div className="flex flex-row justify-center">
+          <Image
+            src={bookImg}
+            alt="book"
+            className="rounded-full"
+            />
+          <div><span className="italic">{book.title}</span></div>
+        </div>
+        <div>Auteur: <span className="italic">{book.author}</span></div>
+        <div className="flex flex-col items-center mt-2">
+          <Link href={`/my-books/${book.id}`}>
+            <button>Modifier</button>
+          </Link>
+        </div>
+      </div>))
   )
 
   return <>
     <h1 className="text-2xl">Mes Livres</h1>
-    {renderBooks()}
+    <div className="flex flex-wrap gap-4 mt-5 mb-5">
+      {renderBooks()}
+    </div>
     <Link href="/my-books/new">
-
       <button>Créer un livre</button>
     </Link>
   </>
