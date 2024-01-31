d2997fd3d416a84fefacd80befed216a5d04fb50
Sylvain Maestri <sylvainmaestri@gmail.com>
2024-01-31 21:30:35 +0100

    configuration next-ui

---
+++ src/components/header/search-input.tsx
@@ -0,0 +1,23 @@
+"use client"
+import { Input } from "@nextui-org/react";
+import { SearchIcon } from "./SearchIcon";
+import { useSearchParams } from "next/navigation";
+
+export default function SearchInput() {
+    const searchParams = useSearchParams();
+    return (
+        <Input
+            name="term"
+            classNames={{
+                base: "max-w-full sm:max-w-[10rem] h-10",
+                mainWrapper: "h-full",
+                input: "text-small",
+                inputWrapper: "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+            }}
+            placeholder="Type to search..."
+            size="sm"
+            startContent={<SearchIcon size={18} />}
+            type="search"
+            defaultValue={searchParams.get("term") || ""}
+        />)
+}
