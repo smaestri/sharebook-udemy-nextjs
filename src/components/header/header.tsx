22a3da7494690ba00f3e0c985c05f136fd7ea223
Sylvain Maestri <sylvainmaestri@gmail.com>
2024-01-31 21:46:08 +0100

    Un peu de style : header

---
+++ src/components/header/header.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import { Button, Navbar, NavbarBrand, NavbarContent, Image } from "@nextui-org/react";
+import SearchInput from "./search-input";
+
+export default async function Header() {
+  return (
+    <Navbar isBordered maxWidth={'full'}>
+      <NavbarContent justify="start">
+        <NavbarBrand className="mr-4">
+          <Link href="/">
+            <Image />
+            <p className="hidden sm:block font-bold text-inherit">Sharebook</p>
+          </Link>
+        </NavbarBrand>
+      </NavbarContent>
+
+      <NavbarContent as="div" className="items-center" justify="end">
+    <Link href="/my-books">Mes livres</Link>
+    <Link href="/list-books">livres</Link>
+    <Link href="/borrows">Mes emprunts</Link>
+        <SearchInput />
+        <Button type="submit">Sign In</Button>
+      </NavbarContent>
+    </Navbar>
+  )
+}
