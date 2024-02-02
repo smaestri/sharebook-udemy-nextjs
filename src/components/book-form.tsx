abc5dc6a928d908e5ee7073a77ef7e3b4b0935c1
Sylvain Maestri <sylvainmaestri@gmail.com>
2024-02-02 22:24:30 +0100

    Repair page new after modify

--- src/components/book-form.tsx
+++ src/components/book-form.tsx
@@ -1,51 +1,15 @@
-import { db } from "@/lib/db"
+"use client"
+import { createOrUpdateBook } from "@/lib/actions";
 import { Input } from "@nextui-org/react";
 import { Book } from "@prisma/client";
-import { redirect } from "next/navigation"
 
 interface BookFormProps {
     book?: Book;
 }
 
 export default async function BookForm({ book }: BookFormProps) {
-    async function createBook(formData: FormData) {
-        "use server"
-        console.log('creating book')
 
-        const title = formData.get('title') as string
-        const author = formData.get('author') as string
-        const book = await db.book.create({
-            data: {
-                title,
-                author
-            }
-        })
-        console.log(book)
-        redirect('/my-books')
-    }
-
-    async function updateBook(formData: FormData) {
-        "use server"
-        const title = formData.get('title') as string
-        const author = formData.get('author') as string
-        console.log('updating book')
-
-        if (!book) {
-            console.error("book not found")
-            return;
-        }
-        const updatedBook = await db.book.update({
-            where: { id: book.id },
-            data: {
-                title,
-                author
-            }
-        })
-        console.log(updatedBook)
-        redirect('/my-books')
-    }
-
-    return (<form action={book ? updateBook : createBook}>
+    return (<form action={createOrUpdateBook.bind(null, book)}>
         <div className="space-y-4">
             <Input name="title" label="Title" defaultValue={book?.title} />
             <Input name="author" label="Author" defaultValue={book?.author} />
---
+++ src/lib/actions.ts
@@ -0,0 +1,36 @@
+"use server"
+
+import { redirect } from "next/navigation"
+import { db } from "./db"
+import { Book } from "@prisma/client"
+
+export async function createOrUpdateBook(book: Book | undefined, formData: FormData) {
+    console.log('creating book')
+
+    const title = formData.get('title') as string
+    const author = formData.get('author') as string
+    const category = formData.get('category') as string
+
+    if (!book) {
+        await db.book.create({
+            data: {
+                title,
+                author,
+                categoryId: parseInt(category),
+            }
+        })
+    } else {
+        await db.book.update({
+            where: { id: book.id },
+            data: {
+                title,
+                author,
+                categoryId: parseInt(category),
+
+            }
+        })
+    }
+
+    console.log(book)
+    redirect('/my-books')
+}
