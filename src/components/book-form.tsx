c07f41638ba7e715d61a0073c19890e3b43540c4
Sylvain Maestri <sylvainmaestri@gmail.com>
2024-02-03 21:31:29 +0100

    Mise en place du hook useformstate

--- src/components/book-form.tsx
+++ src/components/book-form.tsx
@@ -3,17 +3,21 @@ import { BookWithCategory } from "@/app/my-books/[id]/page";
 import { createOrUpdateBook } from "@/lib/actions";
 import { Button, Input, Select, SelectItem } from "@nextui-org/react";
 import { Category } from "@prisma/client";
+import { useFormState } from "react-dom";
 
 interface BookFormProps {
     book?: BookWithCategory;
     categories: Category[]
 }
 
-export default async function BookForm({ book, categories }: BookFormProps) {
+export default function BookForm({ book, categories }: BookFormProps) {
 
     console.log('cate', categories)
+    const [formState, action] = useFormState(createOrUpdateBook.bind(null, book), {
+      message: ""
+    })
 
-    return (<form action={createOrUpdateBook.bind(null, book)}>
+    return (<form action={action}>
         <div className="space-y-4">
             <Input name="title" label="Title" defaultValue={book?.title} />
             <Input name="author" label="Author" defaultValue={book?.author} />
@@ -25,6 +29,9 @@ export default async function BookForm({ book, categories }: BookFormProps) {
               <SelectItem key={category.id} value={category.id} >{category.name}</SelectItem>
             )}
           </Select>
+          <div>
+            {formState.message}
+          </div>
             <Button type="submit">Sauvegarder</Button>
         </div>
     </form>
--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -4,6 +4,8 @@ import { redirect } from "next/navigation"
 import { db } from "./db"
 import { Book } from "@prisma/client"
 
+
+
 export async function deleteBook(id: number) {
     console.log('delete book', id)
     await db.book.delete({
@@ -13,33 +15,37 @@ export async function deleteBook(id: number) {
     redirect('/my-books')
 }    
 
-export async function createOrUpdateBook(book: Book | undefined, formData: FormData) {
-    console.log('creating book')
+export async function createOrUpdateBook(book: Book | undefined , formState: {message: string}, formData: FormData) {
 
-    const title = formData.get('title') as string
-    const author = formData.get('author') as string
-    const category = formData.get('category') as string
-
-    if (!book) {
-        await db.book.create({
-            data: {
-                title,
-                author,
-                categoryId: parseInt(category),
-            }
-        })
-    } else {
-        await db.book.update({
-            where: { id: book.id },
-            data: {
-                title,
-                author,
-                categoryId: parseInt(category),
-
-            }
-        })
+    return {
+        message: "must be longer"
     }
+    // console.log('creating book')
 
-    console.log(book)
-    redirect('/my-books')
+    // const title = formData.get('title') as string
+    // const author = formData.get('author') as string
+    // const category = formData.get('category') as string
+
+    // if (!book) {
+    //     await db.book.create({
+    //         data: {
+    //             title,
+    //             author,
+    //             categoryId: parseInt(category),
+    //         }
+    //     })
+    // } else {
+    //     await db.book.update({
+    //         where: { id: book.id },
+    //         data: {
+    //             title,
+    //             author,
+    //             categoryId: parseInt(category),
+
+    //         }
+    //     })
+    // }
+
+    // console.log(book)
+    // redirect('/my-books')
 }
