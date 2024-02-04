5894b4363b1db719948d9df2c0ef1dab13373484
Sylvain Maestri <sylvainmaestri@gmail.com>
2024-02-04 15:56:27 +0100

    msie a jour du providers next auth

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,15 @@
 // app/providers.tsx
 'use client'
 
-import {NextUIProvider} from '@nextui-org/react'
+import { NextUIProvider } from '@nextui-org/react'
+import { SessionProvider } from 'next-auth/react'
 
-export function Providers({children}: { children: React.ReactNode }) {
+export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <NextUIProvider>
-      {children}
-    </NextUIProvider>
+    <SessionProvider>
+      <NextUIProvider>
+        {children}
+      </NextUIProvider>
+    </SessionProvider>
   )
 }
