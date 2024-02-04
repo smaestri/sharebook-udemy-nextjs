dd14fee60600c3cc79bad5aba962511f30029118
Sylvain Maestri <sylvainmaestri@gmail.com>
2024-02-04 21:52:37 +0100

    Afficher compteur de livres

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import { db } from "@/lib/db";
+import Counter from "./counter";
 import { Category } from "@prisma/client";
 
 export default async function Navigation() {
    const categories = await db.category.findMany();
 
    const renderCategories = categories.map((cat: Category) => {
-     return (<div key={cat.id}><Link href={{ pathname: `/list-books`, query: { categoryId: cat.id } }} >{cat.name}</Link></div>)
+     return (<div key={cat.id}><Link href={{ pathname: `/list-books`, query: { categoryId: cat.id } }} >{cat.name} (<Counter categoryId={cat.id} />)</Link></div>)
    })
   return (<div className="flex flex-col">
     {renderCategories}
